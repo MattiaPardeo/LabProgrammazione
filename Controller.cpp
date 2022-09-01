27355dace198b1e8d520d7dd43ec54f940c4349d
Mattia <mattia.pardeo@stud.unifi.it>
2022-09-01 17:23:47 +0200

    Implementati metodi max, somma e media e modificato per supporto dei float in Controller

--- Controller.cpp
+++ Controller.cpp
@@ -2,52 +2,85 @@
 // Created by mattia on 30/08/22.
 //
 
+#include <numeric>
 #include "Controller.h"
 
 void Controller::min(int rigaIniziale, int rigaFinale, int colonnaIniziale, int colonnaFinale, wxGridTableBase *tab) {
-    std::vector<int> colonna;
+    std::vector<int> celle;
     if (rigaIniziale == rigaFinale) {
         for (int i = colonnaIniziale; i < colonnaFinale; i++) {
-            colonna.push_back(wxAtoi(tab->GetValue(rigaIniziale, i)));
+            celle.push_back(wxAtoi(tab->GetValue(rigaIniziale, i)));
         }
-        int minimo = *std::min_element(colonna.begin(), colonna.end());
-        tab->SetValue(rigaIniziale, colonnaFinale, wxString::Format(wxT("%i"), minimo));
+        float minimo = (float)*std::min_element(celle.begin(), celle.end());
+        tab->SetValue(rigaIniziale, colonnaFinale, wxString::Format(wxT("%g"), minimo)); // %g per non avere cifre decimali se non servono
     }
     if (colonnaIniziale == colonnaFinale) {
         for (int i = rigaIniziale; i < rigaFinale; i++) {
-            colonna.push_back(wxAtoi(tab->GetValue(i, colonnaIniziale)));
+            celle.push_back(wxAtoi(tab->GetValue(i, colonnaIniziale)));
         }
-        int minimo = *std::min_element(colonna.begin(), colonna.end());
-        tab->SetValue(rigaFinale, colonnaIniziale, wxString::Format(wxT("%i"), minimo));
+        float minimo = (float)*std::min_element(celle.begin(), celle.end());
+        tab->SetValue(rigaFinale, colonnaIniziale, wxString::Format(wxT("%g"), minimo));
     }
 
     tabella->setTable(tab);
 }
 
 void Controller::max(int rigaIniziale, int rigaFinale, int colonnaIniziale, int colonnaFinale, wxGridTableBase *tab) {
-    std::vector<int> colonna;
+    std::vector<int> celle;
     if (rigaIniziale == rigaFinale) {
         for (int i = colonnaIniziale; i < colonnaFinale; i++) {
-            colonna.push_back(wxAtoi(tab->GetValue(rigaIniziale, i)));
+            celle.push_back(wxAtoi(tab->GetValue(rigaIniziale, i)));
         }
-        int massimo = *std::max_element(colonna.begin(), colonna.end());
-        tab->SetValue(rigaIniziale, colonnaFinale, wxString::Format(wxT("%i"), massimo));
+        float massimo = (float)*std::max_element(celle.begin(), celle.end());
+        tab->SetValue(rigaIniziale, colonnaFinale, wxString::Format(wxT("%g"), massimo));
     }
     if (colonnaIniziale == colonnaFinale) {
         for (int i = rigaIniziale; i < rigaFinale; i++) {
-            colonna.push_back(wxAtoi(tab->GetValue(i, colonnaIniziale)));
+            celle.push_back(wxAtoi(tab->GetValue(i, colonnaIniziale)));
         }
-        int massimo = *std::max_element(colonna.begin(), colonna.end());
-        tab->SetValue(rigaFinale, colonnaIniziale, wxString::Format(wxT("%i"), massimo));
+        float massimo = (float)*std::max_element(celle.begin(), celle.end());
+        tab->SetValue(rigaFinale, colonnaIniziale, wxString::Format(wxT("%g"), massimo));
     }
 
     tabella->setTable(tab);
 }
 
 void Controller::media(int rigaIniziale, int rigaFinale, int colonnaIniziale, int colonnaFinale, wxGridTableBase *tab) {
+    std::vector<int> celle;
+    if (rigaIniziale == rigaFinale) {
+        for (int i = colonnaIniziale; i < colonnaFinale; i++) {
+            celle.push_back(wxAtoi(tab->GetValue(rigaIniziale, i)));
+        }
+        float media = (float)std::accumulate(celle.begin(), celle.end(), 0)/(float)celle.size();
+        tab->SetValue(rigaIniziale, colonnaFinale, wxString::Format(wxT("%g"), media));
+    }
+    if (colonnaIniziale == colonnaFinale) {
+        for (int i = rigaIniziale; i < rigaFinale; i++) {
+            celle.push_back(wxAtoi(tab->GetValue(i, colonnaIniziale)));
+        }
+        float media = (float)std::accumulate(celle.begin(), celle.end(), 0)/(float)celle.size();
+        tab->SetValue(rigaFinale, colonnaIniziale, wxString::Format(wxT("%g"), media));
+    }
 
+    tabella->setTable(tab);
 }
 
 void Controller::somma(int rigaIniziale, int rigaFinale, int colonnaIniziale, int colonnaFinale, wxGridTableBase *tab) {
+    std::vector<int> celle;
+    if (rigaIniziale == rigaFinale) {
+        for (int i = colonnaIniziale; i < colonnaFinale; i++) {
+            celle.push_back(wxAtoi(tab->GetValue(rigaIniziale, i)));
+        }
+        float somma = (float)std::accumulate(celle.begin(), celle.end(), 0);
+        tab->SetValue(rigaIniziale, colonnaFinale, wxString::Format(wxT("%g"), somma));
+    }
+    if (colonnaIniziale == colonnaFinale) {
+        for (int i = rigaIniziale; i < rigaFinale; i++) {
+            celle.push_back(wxAtoi(tab->GetValue(i, colonnaIniziale)));
+        }
+        float somma = (float)std::accumulate(celle.begin(), celle.end(), 0);
+        tab->SetValue(rigaFinale, colonnaIniziale, wxString::Format(wxT("%g"), somma));
+    }
 
+    tabella->setTable(tab);
 }
