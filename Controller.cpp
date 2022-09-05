3683101cae6a0c1d2ad04fbf830bb98c73bdf66d
Mattia <mattia.pardeo@stud.unifi.it>
2022-09-05 11:19:18 +0200

    Corretto in Controller.cpp il minimo della colonna con double invece che int, lo stesso in max con riga e colonna, modificato i valori di test di Controller

--- Controller.cpp
+++ Controller.cpp
@@ -16,7 +16,7 @@ void Controller::min(int rigaIniziale, int rigaFinale, int colonnaIniziale, int 
     }
     if (colonnaIniziale == colonnaFinale) {
         for (int i = rigaIniziale; i < rigaFinale; i++) {
-            celle.push_back(wxAtoi(tab->GetValue(i, colonnaIniziale)));
+            celle.push_back(wxAtof(tab->GetValue(i, colonnaIniziale)));
         }
         double minimo = *std::min_element(celle.begin(), celle.end());
         tab->SetValue(rigaFinale, colonnaIniziale, wxString::Format(wxT("%g"), minimo));
@@ -29,14 +29,14 @@ void Controller::max(int rigaIniziale, int rigaFinale, int colonnaIniziale, int 
     std::vector<double> celle;
     if (rigaIniziale == rigaFinale) {
         for (int i = colonnaIniziale; i < colonnaFinale; i++) {
-            celle.push_back(wxAtoi(tab->GetValue(rigaIniziale, i)));
+            celle.push_back(wxAtof(tab->GetValue(rigaIniziale, i)));
         }
         double massimo = *std::max_element(celle.begin(), celle.end());
         tab->SetValue(rigaIniziale, colonnaFinale, wxString::Format(wxT("%g"), massimo));
     }
     if (colonnaIniziale == colonnaFinale) {
         for (int i = rigaIniziale; i < rigaFinale; i++) {
-            celle.push_back(wxAtoi(tab->GetValue(i, colonnaIniziale)));
+            celle.push_back(wxAtof(tab->GetValue(i, colonnaIniziale)));
         }
         double massimo = *std::max_element(celle.begin(), celle.end());
         tab->SetValue(rigaFinale, colonnaIniziale, wxString::Format(wxT("%g"), massimo));
--- test/ControllerFixture.cpp
+++ test/ControllerFixture.cpp
@@ -12,10 +12,10 @@ protected:
         tab = new TabellaModel();
         ctrl = new Controller(tab);
         tab->getTable()->SetValue(1, 1, wxString::Format(wxT("%g"), 3.1));
-        tab->getTable()->SetValue(1, 2, wxString::Format(wxT("%g"), 1.0));
-        tab->getTable()->SetValue(1, 3, wxString::Format(wxT("%g"), 5.0));
-        tab->getTable()->SetValue(2, 1, wxString::Format(wxT("%g"), 1.0));
-        tab->getTable()->SetValue(3, 1, wxString::Format(wxT("%g"), 5.0));
+        tab->getTable()->SetValue(1, 2, wxString::Format(wxT("%g"), 1.2));
+        tab->getTable()->SetValue(1, 3, wxString::Format(wxT("%g"), 5.3));
+        tab->getTable()->SetValue(2, 1, wxString::Format(wxT("%g"), 1.2));
+        tab->getTable()->SetValue(3, 1, wxString::Format(wxT("%g"), 5.3));
     }
 
     Controller *ctrl;
@@ -25,47 +25,47 @@ protected:
 TEST_F(ControllerTestSuite, TestMinRiga){
     ctrl->min(1, 1, 1, 4, tab->getTable());
     double result = wxAtof(tab->getTable()->GetValue(1, 4));
-    ASSERT_DOUBLE_EQ(1, result);
+    ASSERT_DOUBLE_EQ(1.2, result);
 }
 
 TEST_F(ControllerTestSuite, TestMaxRiga){
     ctrl->max(1, 1, 1, 4, tab->getTable());
     double result = wxAtof(tab->getTable()->GetValue(1, 4));
-    ASSERT_DOUBLE_EQ(5, result);
+    ASSERT_DOUBLE_EQ(5.3, result);
 }
 
 TEST_F(ControllerTestSuite, TestMediaRiga){
     ctrl->media(1, 1, 1, 4, tab->getTable());
     double result = wxAtof(tab->getTable()->GetValue(1, 4));
-    ASSERT_DOUBLE_EQ(3.03333, result);
+    ASSERT_DOUBLE_EQ(3.2, result);
 }
 
 TEST_F(ControllerTestSuite, TestSommaRiga){
     ctrl->somma(1, 1, 1, 4, tab->getTable());
     double result = wxAtof(tab->getTable()->GetValue(1, 4));
-    ASSERT_DOUBLE_EQ(9.1, result);
+    ASSERT_DOUBLE_EQ(9.6, result);
 }
 
 TEST_F(ControllerTestSuite, TestMinColonna){
     ctrl->min(1, 4, 1, 1, tab->getTable());
     double result = wxAtof(tab->getTable()->GetValue(4, 1));
-    ASSERT_DOUBLE_EQ(1, result);
+    ASSERT_DOUBLE_EQ(1.2, result);
 }
 
 TEST_F(ControllerTestSuite, TestMaxColonna){
     ctrl->max(1, 4, 1, 1, tab->getTable());
     double result = wxAtof(tab->getTable()->GetValue(4, 1));
-    ASSERT_DOUBLE_EQ(5, result);
+    ASSERT_DOUBLE_EQ(5.3, result);
 }
 
 TEST_F(ControllerTestSuite, TestMediaColonna){
     ctrl->media(1, 4, 1, 1, tab->getTable());
     double result = wxAtof(tab->getTable()->GetValue(4, 1));
-    ASSERT_DOUBLE_EQ(3.03333, result);
+    ASSERT_DOUBLE_EQ(3.2, result);
 }
 
 TEST_F(ControllerTestSuite, TestSommaColonna){
     ctrl->somma(1, 4, 1, 1, tab->getTable());
     double result = wxAtof(tab->getTable()->GetValue(4, 1));
-    ASSERT_DOUBLE_EQ(9.1, result);
+    ASSERT_DOUBLE_EQ(9.6, result);
 }
